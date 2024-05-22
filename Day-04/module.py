c982db9a2398aeaaea9423b948717f962e844309
Biswaranjan Biswal <145491866+biswaranjanbiswal@users.noreply.github.com>
2024-05-22 07:00:27 +0530

    Create module.py

---
+++ Day-04/module.py
@@ -0,0 +1,9 @@
+# Import the entire module
+import example as ex
+print (ex.add(5,5))
+print (ex.mul(10,5))
+
+
+# Import any specific feature/function from the  module
+from example import add
+print (add(5,8))
